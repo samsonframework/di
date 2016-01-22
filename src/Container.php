a81b4c8094b3a523cdd238ffde7dd3fff08cf404
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-01-22 18:14:44 -0400

    Added first implementation

--- src/Container.php
+++ src/Container.php
@@ -5,11 +5,50 @@
  */
 namespace samsonframework\di;
 
+use Interop\Container\ContainerInterface;
+use samsonframework\di\exception\NotFoundException;
+
 /**
  * Class Container
  * @package samsonframework\di
  */
-class Container
+class Container implements ContainerInterface
 {
+    /** @var array[string] Collection of loaded modules into container */
+    protected $modules = array();
 
+    /**
+     * Finds an entry of the container by its identifier and returns it.
+     *
+     * @param string $id Identifier of the entry to look for.
+     *
+     * @throws NotFoundException  No entry was found for this identifier.
+     * @throws ContainerException Error while retrieving the entry.
+     *
+     * @return mixed Entry.
+     */
+    public function get($id)
+    {
+        // Set pointer to module
+        $module = &$this->modules[$id];
+
+        if (null === $module) {
+            throw new NotFoundException($id);
+        } else {
+            return $module;
+        }
+    }
+
+    /**
+     * Returns true if the container can return an entry for the given identifier.
+     * Returns false otherwise.
+     *
+     * @param string $id Identifier of the entry to look for.
+     *
+     * @return boolean
+     */
+    public function has($id)
+    {
+        return isset($this->modules[$id]);
+    }
 }
---
+++ src/exception/ContainerException.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by Vitaly Iegorov <egorov@samsonos.com>.
+ * on 22.01.16 at 18:09
+ */
+namespace samsonframework\di\exception;
+
+class ContainerException extends \Exception implements \Interop\Container\Exception\ContainerException
+{
+
+}
