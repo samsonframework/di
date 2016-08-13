89e50adf88c9d9cc25362c2e38c908ca15c009ac
Vitaly Iegorov <vitalyiegorov@gmail.com>
2016-08-13 10:58:23 +0300

    Simplify

--- src/AbstractContainer.php
+++ src/AbstractContainer.php
@@ -131,92 +131,27 @@ abstract class AbstractContainer implements ContainerInterface
     }
 
     /**
-     * Returns true if the container can return an entry for the given identifier.
-     * Returns false otherwise.
+     * Define if container has dependency.
      *
-     * @param string $alias Identifier of the entry to look for.
+     * @param string $dependency Dependency class name or alias
      *
-     * @return boolean
+     * @return bool True if container has requested dependency
      */
-    public function has($alias)
+    public function has($dependency)
     {
-        $found = array_key_exists($alias, $this->dependencies)
-            || in_array($alias, $this->aliases);
+        $found = array_key_exists($dependency, $this->dependencies)
+            || in_array($dependency, $this->aliases, true);
 
+        // Delegate searching if not found
         if (!$found) {
             foreach ($this->delegates as $delegate) {
-                if ($delegate->has($alias)) {
+                if ($delegate->has($dependency)) {
                     return true;
                 }
             }
         }
 
         return $found;
-    }
-
-    /**
-     * Generate logic conditions and their implementation for container and its delegates.
-     *
-     * @param string     $inputVariable Input condition parameter variable name
-     * @param bool|false $started       Flag if condition branching has been started
-     */
-    public function generateConditions($inputVariable = '$alias', $started = false)
-    {
-        // Iterate all container dependencies
-        foreach ($this->dependencies as $alias => $entity) {
-            // Generate condition statement to define if this class is needed
-            $conditionFunc = !$started ? 'defIfCondition' : 'defElseIfCondition';
-
-            // Create condition branch
-            $condition = $inputVariable . ' === \'' . $alias . '\'';
-            // If we have an alias for this - add it to condition
-            $condition .= array_key_exists($alias, $this->aliases)
-                ? ' || ' . $inputVariable . ' === \'' . $this->aliases[$alias] . '\''
-                : '';
-            // Output condition branch
-            $this->generator->$conditionFunc($condition);
-
-            // Generate condition for each dependency
-            $this->generateCondition($alias, $entity);
-
-            // Set flag that condition is started
-            $started = true;
-        }
-
-        /** @var self $delegate Iterate delegated container to get their conditions */
-        foreach ($this->delegates as $delegate) {
-            // Set current generator
-            if ($delegate instanceof AbstractContainer) {
-                $delegate->generator = $this->generator;
-            }
-            $delegate->generateConditions($inputVariable, $started);
-        }
-    }
-
-    /**
-     * Generate dependency injection logic function.
-     *
-     * @param string $functionName
-     *
-     * @return string PHP logic function code
-     */
-    public function generateFunction($functionName = self::LOGIC_FUNCTION_NAME)
-    {
-        $inputVariable = '$aliasOrClassName';
-        $this->generator
-            ->defFunction($functionName, array($inputVariable))
-            ->defVar('static $services')
-            ->defVar($inputVariable)
-            ->newLine();
-
-        // Generate all container and delegate conditions
-        $this->generateConditions($inputVariable, false);
-
-        // Add method not found
-        return $this->generator
-            ->endIfCondition()
-            ->endFunction()
-            ->flush();
     }
 
     /**
@@ -229,12 +164,4 @@ abstract class AbstractContainer implements ContainerInterface
      * @return self Chaining
      */
     abstract public function set($entity, $alias = null, array $parameters = array());
-
-    /**
-     * Generate container dependency condition code.
-     *
-     * @param string $alias  Entity alias
-     * @param mixed  $entity Entity
-     */
-    abstract protected function generateCondition($alias, &$entity);
 }
--- src/Container.php
+++ src/Container.php
@@ -8,96 +8,14 @@ namespace samsonframework\di;
 use samsonframework\di\exception\ClassNotFoundException;
 use samsonframework\di\exception\ConstructorParameterNotSetException;
 
+/**
+ * Class Container
+ * @package samsonframework\di
+ */
 class Container extends AbstractContainer
 {
-    /** Variable name for storing services */
-    const STATIC_COLLECTION_VARIABLE = '$services';
-
     /** @var array[string] Collection of loaded services */
     protected $services = array();
-
-    /**
-     * Get reflection parameter class name type hint if present without
-     * auto loading and throwing exceptions.
-     * TODO: Add resolving configured classes
-     *
-     * @param \ReflectionParameter $param Parameter for parsing
-     *
-     * @return string|null Class name typehint or null
-     */
-    protected function getClassName(\ReflectionParameter $param)
-    {
-        preg_match('/\[\s\<\w+?>\s(?<class>[\w\\\\]+)/', (string)$param, $matches);
-        return array_key_exists('class', $matches) && $matches['class'] !== 'array'
-            ? '\\' . ltrim($matches[1], '\\')
-            : null;
-    }
-
-    /**
-     * Analyze class construtor dependencies and create tree for nested dependencies.
-     *
-     * @param \ReflectionMethod $constructor Reflection method __constructor instance
-     * @param string $className
-     * @param array  $dependencies Reference to tree for filling up
-     *
-     * @throws ClassNotFoundException
-     */
-    protected function buildConstructorDependencies(\ReflectionMethod $constructor, $className, &$dependencies)
-    {
-        // Iterate all dependencies
-        foreach ($constructor->getParameters() as $parameter) {
-            // Ignore optional parameters
-            if (!$parameter->isOptional()) {
-                // Read dependency class name
-                $dependencyClass = $this->getClassName($parameter);
-
-                // Set pointer to parameter as it can be set before
-                $parameterPointer = &$dependencies[$className][$parameter->name];
-
-                // If we have found dependency class
-                if ($dependencyClass !== null) {
-                    // Point dependency class name
-                    $parameterPointer = $dependencyClass;
-                    // Go deeper in recursion and pass new branch there
-                    $this->buildDependenciesTree($dependencyClass, $dependencies);
-                }
-            } else { // Stop iterating as first optional parameter is met
-                break;
-            }
-        }
-    }
-
-    /**
-     * Recursively build class constructor dependencies tree.
-     * TODO: Analyze recurrent dependencies and throw an exception
-     *
-     * @param string $className    Current class name for analyzing
-     * @param array  $dependencies Reference to tree for filling up
-     *
-     * @return array [string] Multidimensional array as dependency tree
-     * @throws ClassNotFoundException
-     */
-    protected function buildDependenciesTree($className, array &$dependencies)
-    {
-        // Resolve class name if present
-        $className = array_key_exists($className, $this->resolver)
-            ? $this->resolver[$className]
-            : $className;
-
-        // We need this class to exists to use reflections, it will try to autoload it also
-        if (class_exists($className)) {
-            $class = new \ReflectionClass($className);
-            // We can build dependency tree only from constructor dependencies
-            $constructor = $class->getConstructor();
-            if (null !== $constructor) {
-                $this->buildConstructorDependencies($constructor, $className, $dependencies);
-            }
-        } else { // Something went wrong and class is not auto loaded and missing
-            throw new ClassNotFoundException($className);
-        }
-
-        return $dependencies;
-    }
 
     /**
      * Set service dependency. Upon first creation of this class instance
@@ -127,99 +45,10 @@ class Container extends AbstractContainer
      */
     public function set($className, $alias = null, array $parameters = array())
     {
-        // Add this class dependencies to dependency tree
-        $this->dependencies = array_merge(
-            $this->dependencies,
-            $this->buildDependenciesTree($className, $this->dependencies)
-        );
-
         // Merge other class constructor parameters
         $this->dependencies[$className] = array_merge($this->dependencies[$className], $parameters);
 
         // Store alias for this class name
         $this->aliases[$className] = $alias;
-    }
-
-    /**
-     * Generate initial class instance declaration
-     * @param string $className Entity class name
-     */
-    protected function generateInitialDeclaration($className)
-    {
-        if (array_key_exists($className, $this->services)) {
-            // Start service search or creation
-            $this->generator
-                ->text('isset('.self::STATIC_COLLECTION_VARIABLE.'[\''.$className.'\'])')
-                ->newLine('? '.self::STATIC_COLLECTION_VARIABLE.'[\''.$className.'\']')
-                ->newLine(': '.self::STATIC_COLLECTION_VARIABLE.'[\''.$className.'\'] = new '.$className.'(')->tabs++;
-        } else {
-            // Start object instance creation
-            $this->generator->text('new ' . $className . '(')->tabs++;
-        }
-    }
-
-    /**
-     * Parse class dependencies generated by dependency tree.
-     *
-     * @param mixed $dependency Dependency value
-     * @param string $variable Dependency name
-     * @param string $className
-     * @param int $level Current recursion level
-     *
-     * @throws ConstructorParameterNotSetException
-     */
-    protected function parseClassDependencies($dependency, $variable, $className, $level)
-    {
-        // If dependency value is a string
-        if (is_string($dependency)) {
-            // Define if we have this dependency described in dependency tree
-            $dependencyPointer = &$this->dependencies[$dependency];
-            if (null !== $dependencyPointer) {
-                // We have dependencies tree for this entity
-                $this->generateCondition($dependency, $dependencyPointer, $level + 1);
-            } elseif (class_exists($dependency, false)) {
-                // There are no dependencies for this entity
-                $this->generator->newLine('new ' . $dependency . '()');
-            } else { // String variable
-                $this->generator->newLine()->stringValue($dependency);
-            }
-        } elseif (is_array($dependency)) { // Dependency value is array
-            $this->generator->newLine()->arrayValue($dependency);
-        } elseif ($dependency === null) { // Parameter is not set
-            throw new ConstructorParameterNotSetException($className . '::' . $variable);
-        }
-    }
-
-    /**
-     * Generate container dependency condition code.
-     *
-     * @param string $className
-     * @param mixed  $dependencies
-     * @param int    $level
-     *
-     * @throws ConstructorParameterNotSetException
-     *
-     */
-    protected function generateCondition($className, &$dependencies, $level = 0)
-    {
-        $this->generator->newLine(($level === 0) ? 'return ' : '');
-
-        $this->generateInitialDeclaration($className);
-
-        // Get last dependency variable name
-        end($dependencies);
-        $last = key($dependencies);
-
-        // Iterate all dependencies for this class
-        foreach ($dependencies as $variable => $dependency) {
-            $this->parseClassDependencies($dependency, $variable, $className, $level);
-
-            // Add comma if this is not last dependency
-            if ($variable !== $last) {
-                $this->generator->text(',');
-            }
-        }
-        $this->generator->tabs--;
-        $this->generator->newLine(')' . ($level === 0 ? ';' : ''));
     }
 }
